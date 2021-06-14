1bcfa25efad3654fef2b34012ee9fd165052d99d
kibria <shan2neo@gmail.com>
2021-06-14 22:29:29 +0600

    bug error

--- src/Question.js
+++ src/Question.js
@@ -5,8 +5,6 @@ const Question = ({title,info}) => {
   const [showAnswer,setShowAnswer] = useState(false);
   return (  <article className="question">
     <header> 
-      <h4>{title}</h4> 
-      <button className="btn" onClick={()=>{setShowAnswer(!showAnswer);}}> {showAnswer?< AiOutlineMinus />: < AiOutlinePlus/>} </button>
         
     </header>
    { showAnswer &&  <p>{info}</p>}
