2b0e0b5734d4fe8444c6cc3361204f64c08a8315
kibria <shan2neo@gmail.com>
2021-06-14 06:40:08 +0600

    accordion question

--- src/Question.js
+++ src/Question.js
@@ -1,1 +1,13 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = ({title,info}) => {
 
+  const [showAnswer,setShowAnswer] = useState(false);
+  return (  <article className="question">
+    <header> 
+      <h4>{title}</h4> 
+      
+  </article> );
+};
+
+export default Question;
